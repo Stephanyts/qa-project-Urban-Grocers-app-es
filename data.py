3f0be11bc3956a0275bc2e4c73f5ea5a051c1614
Stephany Torres <stephanyts54@gmail.com>
2024-08-06 14:28:54 -0500

    data.py

---
+++ data.py
@@ -0,0 +1,15 @@
+headers = {
+    "Content-Type": "application/json",
+    "authToken": "jknnFApafP4awfAIFfafam2fma",
+    "Autorization": "Bearer {authToken}"
+}
+
+user_body = {
+    "firstName": "Max",
+    "phone": "+10005553535",
+    "address": "8042 Lancaster Ave.Hamburg, NY"
+}
+
+kit_body = {
+    "name": "Mi conjunto"
+}
